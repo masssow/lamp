89d4181fcf311a02274010549775947292221d66
Samba SOW <masssowcode@gmail.com>
2022-04-10 01:16:36 +0200

    "mise en place du sass"

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,16 +4,17 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('message')
+            ->add('message', TextType::class)
             ->add('email', EmailType::class)
         ;
     }
