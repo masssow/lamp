76d9bb9f86d262577a78a72bccb91aeef4e6c0cc
Samba SOW <masssowcode@gmail.com>
2022-04-10 02:23:43 +0200

    "test office"

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -50,7 +50,7 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('app_admin_article_index'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
