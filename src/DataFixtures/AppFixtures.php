8234ad1b43588f29d6dfb39d443021fbd75d8551
Samba SOW <masssowcode@gmail.com>
2022-06-22 16:01:04 +0200

    "fixtures"

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,19 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+ 
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+         
+        // $utilisateur = new Utilisateur();
+      
+        // $manager->persist($utilisateur);
 
-        $manager->flush();
+        // $manager->flush();
     }
 }
